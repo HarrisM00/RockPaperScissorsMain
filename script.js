f3d1c7c35acffc07ab02abcdce2274c1f17cb20d
HarrisM00 <Harrismahmood014@gmail.com>
2024-03-04 14:26:53 -0500

    functions and constants added

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+// Store HTML elements as JS Variables so we can easily access them later:
+// Classes of elements to store (.p-score; .c-score; result)
+// Use document.querySelector() syntax
+const playerScoreDisplay = document.querySelector('.p-score');
+const computerScoreDisplay = document.querySelector('.c-score');
+const resultDisplay = document.querySelector('.result');
+
 let playerScore = 0;
 let computerScore = 0;
+
+// Function to generate computer's choice (rock, paper, or scissors)
+function computerSelection() {
+    const choices = ['rock', 'paper', 'scissors'];
+    // Generate a random value for the computer's choice and store it in a variable
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
